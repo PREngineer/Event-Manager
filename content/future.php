020eb189d6b1a36cf8cef4df9b6d6762c94a66d8
Jorge Pabon <pianistapr@hotmail.com>
2017-10-23 00:07:30 -0400

    Future events handling
    Display message if there are none.

--- content/future.php
+++ content/future.php
@@ -22,49 +22,59 @@ $events = get_FutureEvents();
 <div class="container">
 
 <?php
-
-foreach ($events as $name => $value)
+if( sizeof($events) == 0 )
 {
-  $url = 'http://' . $_SERVER['HTTP_HOST'] . '/content/index.php?action=rsvp?id=' . $value[0];
-
   echo '
-  <div class="col-sm-3">
-    <div class="thumbnail">
-      <img src="../images/event.png" alt="Event image" width="150" height="150">
-      <div class="caption">
-          <table class="table">
-            <tr>
-              <td class="text-center" colspan="2"><h4>' . $value[1] . '</h4></td>
-            </tr>
-            <tr>
-              <td>Date:</td>
-              <td>' . $value[2] . '</td>
-            </tr>
-            <tr>
-              <td>Starts:</td>
-              <td>' . $value[3] . '</td>
-            </tr>
-            <tr>
-              <td>Ends:</td>
-              <td>' . $value[4] . '</td>
-            </tr>
-            <tr>
-              <td>Location:</td>
-              <td>' . $value[5] . '</td>
-            </tr>
-            <tr>
-              <td class="text-center">
-                <a href="?action=RSVP&id=' . $value[0] . '" class="btn btn-primary" role="button">RSVP</a>
-              </td>
-              <td>
-                <a href="mailto:?subject=Thought%20you%20would%20like%20to%20know&body=Check%20out%20this%20event.%20%20' . $url . '" class="btn btn-success" role="button">Share</a>
-              </td class="text-center">
-            </tr>
-          </table>
-      </div>
-    </div>
+  <div class="container">
+    <h3>There are no scheduled events in the future.</h3>
   </div>
   ';
+}
+else
+{
+  foreach ($events as $name => $value)
+  {
+    $url = 'http://' . $_SERVER['HTTP_HOST'] . '/content/index.php?action=rsvp?id=' . $value[0];
+
+    echo '
+    <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="../images/event.png" alt="Event image" width="150" height="150">
+        <div class="caption">
+            <table class="table">
+              <tr>
+                <td class="text-center" colspan="2"><h4>' . $value[1] . '</h4></td>
+              </tr>
+              <tr>
+                <td>Date:</td>
+                <td>' . $value[2] . '</td>
+              </tr>
+              <tr>
+                <td>Starts:</td>
+                <td>' . $value[3] . '</td>
+              </tr>
+              <tr>
+                <td>Ends:</td>
+                <td>' . $value[4] . '</td>
+              </tr>
+              <tr>
+                <td>Location:</td>
+                <td>' . $value[5] . '</td>
+              </tr>
+              <tr>
+                <td class="text-center">
+                  <a href="?action=RSVP&id=' . $value[0] . '" class="btn btn-primary" role="button">RSVP</a>
+                </td>
+                <td>
+                  <a href="mailto:?subject=Thought%20you%20would%20like%20to%20know&body=Check%20out%20this%20event.%20%20' . $url . '" class="btn btn-success" role="button">Share</a>
+                </td class="text-center">
+              </tr>
+            </table>
+        </div>
+      </div>
+    </div>
+    ';
+  }
 }
 
 ?>
