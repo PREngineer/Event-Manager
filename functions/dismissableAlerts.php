489ae354494ee827196b0055716f454a5faa3d72
Jorge Pabon <pianistapr@hotmail.com>
2018-05-09 17:54:04 -0400

    testing

--- functions/dismissableAlerts.php
+++ functions/dismissableAlerts.php
@@ -330,7 +330,7 @@ The alerts are dismissible but they disappear after 5 a seconds with an upper sc
     // Retrieve the codes for this event
     $res = user_RSVP($_POST['EID'], $_GET['id']);
 
-    echo '<script>alert("Posted and ran DB function.");</script>';
+    echo '<script>alert("Posted");</script>';
 
     if( $res == 1)
     {
