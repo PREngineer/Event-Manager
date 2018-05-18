13b324d070d5cb694ae3aef56da59048c71d06f4
Jorge Pabon <pianistapr@hotmail.com>
2018-05-18 17:22:09 -0400

    Creating installer

---
+++ install.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+# Script Name: Event-Manager installer
+# Author: PREngineer (Jorge Pabon) - pianistapr@hotmail.com
+# Publisher: Jorge Pabon
+# License: Personal Use (1 device) per payment
+###########################################################
+
+#-----------------------------------------------------------------------------
+
+# Show a progress bar for $1 seconds
+#
+# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
+# GPL licensed (see end of file) * Use at your own risk!
+#
+# Example: progress_bar 60
+#
+
+progress_bar()
+{
+  local DURATION=$1
+  local INT=0.25      # refresh interval
+
+  local TIME=0
+  local CURLEN=0
+  local SECS=0
+  local FRACTION=0
+
+  local FB=2588       # full block
+
+  trap "echo -e $(tput cnorm); trap - SIGINT; return" SIGINT
+
+  echo -ne "$(tput civis)\r$(tput el)│"                # clean line
+
+  local START=$( date +%s%N )
+
+  while [ $SECS -lt $DURATION ]; do
+    local COLS=$( tput cols )
+
+    # main bar
+    local L=$( bc -l <<< "( ( $COLS - 5 ) * $TIME  ) / ($DURATION-$INT)" | awk '{ printf "%f", $0 }' )
+    local N=$( bc -l <<< $L                                              | awk '{ printf "%d", $0 }' )
+
+    [ $FRACTION -ne 0 ] && echo -ne "$( tput cub 1 )"  # erase partial block
+
+    if [ $N -gt $CURLEN ]; then
+      for i in $( seq 1 $(( N - CURLEN )) ); do
+        echo -ne \\u$FB
+      done
+      CURLEN=$N
+    fi
+
+    # partial block adjustment
+    FRACTION=$( bc -l <<< "( $L - $N ) * 8" | awk '{ printf "%.0f", $0 }' )
+
+    if [ $FRACTION -ne 0 ]; then
+      local PB=$( printf %x $(( 0x258F - FRACTION + 1 )) )
+      echo -ne \\u$PB
+    fi
+
+    # percentage progress
+    local PROGRESS=$( bc -l <<< "( 100 * $TIME ) / ($DURATION-$INT)" | awk '{ printf "%.0f", $0 }' )
+    echo -ne "$( tput sc )"                            # save pos
+    echo -ne "\r$( tput cuf $(( COLS - 6 )) )"         # move cur
+    echo -ne "│ $PROGRESS%"
+    echo -ne "$( tput rc )"                            # restore pos
+
+    TIME=$( bc -l <<< "$TIME + $INT" | awk '{ printf "%f", $0 }' )
+    SECS=$( bc -l <<<  $TIME         | awk '{ printf "%d", $0 }' )
+
+    # take into account loop execution time
+    local END=$( date +%s%N )
+    local DELTA=$( bc -l <<< "$INT - ( $END - $START )/1000000000" \
+                   | awk '{ if ( $0 > 0 ) printf "%f", $0; else print "0" }' )
+    sleep $DELTA
+    START=$( date +%s%N )
+  done
+
+  echo $(tput cnorm)
+  trap - SIGINT
+}
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
+#-----------------------------------------------------------------------------
+
+# Color definition variables
+YELLOW='\e[33;3m'
+RED='\e[91m'
+BLACK='\033[0m'
+CYAN='\e[96m'
+GREEN='\e[92m'
+
+# Display the Title Information
+echo
+echo -e $RED
+echo -e "╔══════════════════════════════════════════════════════════════╗"
+echo -e "║███████╗██████╗ ██████╗ ██╗███╗   ██╗████████╗███████╗██████╗ ║"
+echo -e "║██╔════╝██╔══██╗██╔══██╗██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗║"
+echo -e "║███████╗██████╔╝██████╔╝██║██╔██╗ ██║   ██║   █████╗  ██████╔╝║"
+echo -e "║╚════██║██╔═══╝ ██╔══██╗██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗║"
+echo -e "║███████║██║     ██║  ██║██║██║ ╚████║   ██║   ███████╗██║  ██║║"
+echo -e "║╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝║"
+echo -e "╚══════════════════════════════════════════════════════════════╝"
+echo -e $CYAN"          ╔Brought to you by PREngineer (Jorge Pabón)╗     "
+echo -e $CYAN"          ╚════https://www.github.com/PREngineer═════╝     "
+echo
+echo -e $GREEN'Installer'$BLACK
+
+echo
+echo -e $YELLOW"|--> Updating Ubuntu..."
+echo
+
+sudo apt-get -y update &>/dev/null & progress_bar
+sudo apt-get -y upgrade &>/dev/null & progress_bar
+
+exit 0
