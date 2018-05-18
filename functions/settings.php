4b8cc63af910a8a8ce862d3ca2af7115295fa57a
Jorge Pabon <pianistapr@hotmail.com>
2018-05-18 17:23:01 -0400

    test

--- functions/settings.php
+++
@@ -1,9 +0,0 @@
-
-    <?php
-      $DBUSER = "root";
-      $DBPASS = "root";
-      $DB     = "Event_Manager";
-      $DBHOST = "localhost";
-      $DBPORT = "3306";
-      $DBTYPE = "mysql";
-    ?>
